065f038bdc8ea30cae9c01b1453d6009e21b5461
Marcelo Pottier <marcelopottier@gmail.com>
2022-11-01 10:28:43 -0300

    Criação de tasks react

---
+++ react-demo/src/components/Task.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Task.css";
+const Task = ({task, handleTaskClick, handleTaskDeletion}) => {
+    return (
+        <div className='task-container' style={task.completed ? {borderLeft: "6px solid chartreuse"} : {}}>
+            <div className='task-title' onClick={() => handleTaskClick(task.id)}>
+                {task.title}
+            </div>
+            <div className='buttons-container'>
+                <button className='remove-task-button' onClick={() => handleTaskDeletion(task.id)}>X</button>
+            </div>
+        </div>
+    );
+}
+
+export default Task;
